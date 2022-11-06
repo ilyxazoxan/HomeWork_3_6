5f589aa09631aaa4d1842aeae0a2c9800f38ce25
Ilya Danilov <esozap32@mail.ru>
2022-11-06 14:30:02 +0400

    SimpleNumber

--- src/main/java/SimpleNumber.java
+++ src/main/java/SimpleNumber.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class SimpleNumber {
+public class SimpleNumber {
+    public static void main(String args[]) {
+
+        int i, j;
+        boolean isPrime;
+        for (i = 2; i < 100; i++) {
+            isPrime = true;
+            // проверить, делится ли число без остатка
+            for (j = 2; j <= i / j; j++)
+                // если число делится без остатка, значит, оно не простое
+                if ((i % j) == 0) isPrime = false;
+            if (isPrime)
+                System.out.println(i + " - простое число.");
+        }
+    }
+
 }
